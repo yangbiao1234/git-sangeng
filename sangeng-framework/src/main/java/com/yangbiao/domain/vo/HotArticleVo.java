2101dbf4d7ebeeea0147511ef0abb7b9d6fcb06e
yangbiao <3200410953@qq.com>
2023-06-10 22:05:33 +0800

    2023年6月10日完成

---
+++ sangeng-framework/src/main/java/com/yangbiao/domain/vo/HotArticleVo.java
@@ -0,0 +1,21 @@
+package com.yangbiao.domain.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HotArticleVo {
+    //返回给前段的字段
+
+    //id
+    private Long id;
+
+    //标题
+    private String title;
+
+    //访问量
+    private Long viewCount;
+}
--- sangeng-framework/src/main/java/com/yangbiao/service/impl/ArticleServiceImpl.java
+++ sangeng-framework/src/main/java/com/yangbiao/service/impl/ArticleServiceImpl.java
@@ -3,12 +3,17 @@ package com.yangbiao.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mysql.cj.util.StringUtils;
+import com.yangbiao.constants.SystemConstants;
 import com.yangbiao.domain.ResponseResult;
 import com.yangbiao.domain.entity.Article;
+import com.yangbiao.domain.vo.HotArticleVo;
 import com.yangbiao.mapper.ArticleMapper;
 import com.yangbiao.service.ArticleService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -21,18 +26,32 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, Article> impl
 
         LambdaQueryWrapper<Article> queryWrapper = new LambdaQueryWrapper<Article>();
         //必须是正式文章(方法引用)
-        queryWrapper.eq(Article::getStatus,0);
+        queryWrapper.eq(Article::getStatus, SystemConstants.ARTICLE_STATUS_NORMAL);
         //按照浏览量进行排序
-        queryWrapper.orderByAsc(Article::getViewCount);
+        queryWrapper.orderByDesc(Article::getViewCount);
         //最多只查询10条记录(分页第一页查询10条记录)
         Page<Article> page = new Page<Article>(1,10);
         page(page, queryWrapper);
 
+        //符合检索条件对象的所有字段
         List<Article> records = page.getRecords();
+        //在次包装
+        List<HotArticleVo> articleVos = new ArrayList<HotArticleVo>();
+        //bean拷贝
+        //records符合查询条件的对象的所有字段过于敏感 用vo在次包装
+        for (Article record : records) {
+            HotArticleVo vo = new HotArticleVo();
+            BeanUtils.copyProperties(record, vo);
+            articleVos.add(vo);
+        }
 
 
 
-        return ResponseResult.okResult(records);
+
+
+
+
+        return ResponseResult.okResult(articleVos);
     }
 
 
