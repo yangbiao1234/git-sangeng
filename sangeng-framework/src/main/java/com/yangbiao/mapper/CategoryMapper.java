df27f64d57408e43e4d085d3cf7677176727ea64
yangbiao <3200410953@qq.com>
2023-06-14 21:18:11 +0800

    分类表(Category)表数据库访问层

---
+++ sangeng-framework/src/main/java/com/yangbiao/mapper/CategoryMapper.java
@@ -0,0 +1,16 @@
+package com.yangbiao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yangbiao.domain.entity.Category;
+
+
+/**
+ * 分类表(Category)表数据库访问层
+ *
+ * @author makejava
+ * @since 2023-06-14 21:09:17
+ */
+public interface CategoryMapper extends BaseMapper<Category> {
+
+}
+
