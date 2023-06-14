a86e8940bf55aa61cb904e1d3182dbd2892500ba
yangbiao <3200410953@qq.com>
2023-06-14 21:18:53 +0800

    分类表(Category)表服务接口

---
+++ sangeng-framework/src/main/java/com/yangbiao/service/CategoryService.java
@@ -0,0 +1,16 @@
+package com.yangbiao.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yangbiao.domain.entity.Category;
+
+
+/**
+ * 分类表(Category)表服务接口
+ *
+ * @author makejava
+ * @since 2023-06-14 21:09:20
+ */
+public interface CategoryService extends IService<Category> {
+
+}
+
