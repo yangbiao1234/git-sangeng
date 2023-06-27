294b2ac0a8a243bedd274f9bc1e427d8778e10af
yangbiao <3200410953@qq.com>
2023-06-28 07:38:21 +0800

    主页和分类页文章的实现

--- sangeng-blog/src/main/java/com/yangbiao/controller/BlogLoginController.java
+++ sangeng-blog/src/main/java/com/yangbiao/controller/BlogLoginController.java
@@ -3,8 +3,11 @@ package com.yangbiao.controller;
 
 import com.yangbiao.domain.ResponseResult;
 import com.yangbiao.domain.entity.User;
+import com.yangbiao.enums.AppHttpCodeEnum;
+import com.yangbiao.exception.SystemException;
 import com.yangbiao.service.BlogLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,10 @@ public class BlogLoginController {
 
     @PostMapping("/login")
     public ResponseResult login(@RequestBody User user){
+        if(!StringUtils.hasText(user.getUserName())){
+            //提示必须要输入用户名  自定义异常
+            throw new SystemException(AppHttpCodeEnum.REQUIRE_USERNAME);
+        }
         return blogLoginService.login(user);
     }
 }
---
+++ sangeng-framework/src/main/java/com/yangbiao/exception/SystemException.java
@@ -0,0 +1,26 @@
+package com.yangbiao.exception;
+
+import com.yangbiao.enums.AppHttpCodeEnum;
+
+//自定义异常
+public class SystemException extends RuntimeException{
+
+    private int code;
+
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public SystemException(AppHttpCodeEnum httpCodeEnum) {
+        super(httpCodeEnum.getMsg());
+        this.code = httpCodeEnum.getCode();
+        this.msg = httpCodeEnum.getMsg();
+    }
+    
+}
